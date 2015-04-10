1d91ba1d87d8a606bc5c88f46a980a7ba06bc787
MiguelGenez <mgenezzeneo@gmail.com>
2015-04-09 22:07:31 -0400

    Tests de login, usuario y proyecto [EN PROCESO]

--- inicio/tests.py
+++ inicio/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from usuario import views
+from django.contrib.auth.models import User
+from django.test import Client
 
-# Create your tests here.
+
+class InicioTestCase(TestCase):
+    
+    def setUp(self):
+        """
+        Funcion que inicializa el test con Datos de Prueba sobre usuarios
+        sobre datos de la BD
+        """
+        self.client = Client()
+        self.username = 'admin'
+        self.email = 'admin@gmail.com'
+        self.password = 'admin'
+        self.test_user = User.objects.create_user(self.username, self.email, self.password)
+    
+    def test_inicio(self):
+        """
+            Test para verificar si va a la pagina de inicio
+        """
+        resp = self.client.get("/inicio/")
+        self.assertEqual(resp.status_code, 200)
+        print('Test inicio exitoso')
+        
+ 
+    def test_login(self):
+        """
+            Test para verificar si realiza el login
+        """  
+        resp = self.client.get('/inicio/')
+        self.assertEqual(resp.status_code, 200)
+        resp= self.client.post('/inicio/', {'username': 'nabil', 'password': '123'})
+        self.assertEqual(resp.status_code, 200)
+        print('Test login exitoso')
+        
+    def test_logout(self):
+        """
+            Test para verificar si realiza el logout
+        """  
+        resp = self.client.get('/inicio/salir/')
+        self.assertEqual(resp.status_code, 200)
+        print('Test logout exitoso')
--- usuario/tests.py
+++ usuario/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.test import Client
+from django.contrib.auth.models import User
+from usuario import views
+from django.test.client import RequestFactory
 
-# Create your tests here.
+class UsuarioTestCase(TestCase):
+    
+    def setUp(self):
+        """
+        Funcion que inicializa el test con Datos de Prueba sobre usuarios
+        """
+        self.factory = RequestFactory
+        self.user =  User.objects.create_user(
+            username='admin', email='admin@doamin.com', password='admin')
+        
+        
+    def test_crea_usuario(self):
+        u = User.objects.create_user('user1', 'user1@domain.com', 'user1')
+        self.assertTrue(isinstance(u,User))
+        print('Test de crear usuario, exitoso')
+        
+

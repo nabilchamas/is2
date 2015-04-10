acbf3780c8628148313e51f7066559099d920474
MiguelGenez <mgenezzeneo@gmail.com>
2015-04-10 15:06:10 -0400

    Tests

--- perfil/tests.py
+++ perfil/tests.py
@@ -3,3 +3,11 @@ from django.contrib.auth.models import User, Group
 
 class PerfilTestCase(TestCase):
     
+    def test_crea_perfil(self):
+        
+        """
+            Test para verificar la correcta creacion de un rol
+        """
+        u = Group.objects.create(name = "ADMINISTRACION" )
+        self.assertTrue(isinstance(u,Group))
+        print('Test de crear perfil, exitoso')
--- proyecto/tests.py
+++ proyecto/tests.py
@@ -2,8 +2,12 @@ from django.test import TestCase
 from proyecto.models import Proyecto
 from proyecto.forms import ProyectoModelForm
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User, Group
 
 class ProyectoTestCase(TestCase):
+    """
+        Encapsula los tests de proyecto
+    """
     
     
     def crear_proyecto(self, nombre="SGP", codigo="123", descripcion="Proyecto para la materia IS2", cliente="Carolina"):
@@ -11,7 +15,7 @@ class ProyectoTestCase(TestCase):
             Para crear un proyecto
         """        
         return Proyecto.objects.create(nombre=nombre, codigo=codigo, descripcion=descripcion, cliente=cliente)
-
+ 
     def test_creacion_proyecto(self):
         """
             Test para verificar la correcta creacion de un proyecto
@@ -20,7 +24,7 @@ class ProyectoTestCase(TestCase):
         self.assertTrue(isinstance(p, Proyecto))
         self.assertEqual(p.__str__(), p.nombre)
         print("Test de creacion de proyecto exitoso")
-
+ 
 #     def test_formulario_valido(self):
 # 
 #         """
@@ -31,22 +35,22 @@ class ProyectoTestCase(TestCase):
 #         form = ProyectoModelForm(data=data)
 #         self.assertTrue(form.is_valid())
 #         print("Test de formulario de proyecto valido exitoso")
-    
+     
     def test_formulario_invalido(self):
-        
+         
         """
             Test para verificar cuando un formulario de creacion de proyectos es invalido
         """
-        
+         
         p = Proyecto.objects.create(nombre="X", codigo="321", descripcion="Proyecto X", cliente="Miguel")
         data = {'nombre': p.nombre, 'codigo': p.codigo, 'descripcion': p.descripcion , 'descripcion': p.descripcion }
         form = ProyectoModelForm(data=data)
         self.assertFalse(form.is_valid())
         print("Test de formulario de proyecto invalido exitoso")
-        
-        
+         
+         
     def test_llamada_a_view_negar_anonimo(self):
-       
+        
         """
             Test para verificar que se impide el acceso al apartado proyecto
             como usuario anonimo
@@ -56,4 +60,3 @@ class ProyectoTestCase(TestCase):
         response = self.client.post('/proyecto/', follow=True)
         self.assertRedirects(response, '/inicio/?next=/proyecto/')
         print("Test de restriccion de acceso a proyecto de un usuario anonimo exitoso")
-        

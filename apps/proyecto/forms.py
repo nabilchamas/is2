a0da88c1ad3c7ef6505f83367e434b0462e2fbfd
nabilchamas <nabilchamasi@gmail.com>
2015-04-20 11:27:29 -0400

    modelo de proyecto actualizado

--- apps/proyecto/forms.py
+++ apps/proyecto/forms.py
@@ -4,4 +4,11 @@ from django import forms
 class ProyectoModelForm(forms.ModelForm):
 	class Meta:
 		model = Proyecto
-		fields = ('nombre', 'codigo', 'descripcion', 'cliente')
+		fields ='__all__'
+
+		widgets = {
+			'fecha_inicio': forms.DateInput(attrs={'class':'datepicker'}),
+			'fecha_fin': forms.DateInput(attrs={'class':'datepicker'}),
+		}
+
+
--- apps/proyecto/models.py
+++ apps/proyecto/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Proyecto(models.Model):
 	nombre = models.CharField(max_length=200)
 	codigo = models.IntegerField(default=0)
-	descripcion = models.CharField(max_length=200)
+	fecha_inicio = models.DateField(null=True)
+	fecha_fin = models.DateField(null=True)
 	cliente = models.CharField(max_length=200)
+	descripcion = models.TextField(max_length=200)
 
 	def __str__(self):
 		return self.nombre
